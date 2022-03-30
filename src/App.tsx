27fc24a9895e2d7f4c033b7e5555afbaa2a22c95
Damian Cygan <damiancygan@MacBook-Pro-Damian.local>
2022-03-30 16:34:40 +0200

    infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,73 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-// import GetFromAPI from './getFromAPI';
-
-const GRAPH_QUERY = `
-  {
-    launchesPast(limit: 10) {
-      mission_name
-      id
-      rocket {
-        rocket_name
-        rocket_type
-      }
-    }
-  `;
+import { IoRocket } from "react-icons/io5";
 
 function App() {
-  const [page, setPage] = useState(0);
-  const [users, setUsers] = useState<any[]>([]);
+  const [page, setPage] = useState(1);
+  const [launches, setLaunches] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const handleScroll = (e: any) => {
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
     const positionOfScrollTop = e.currentTarget; //position of scroll
     const clientHeight = e.currentTarget; //size of client screen
     const scrollHeight = e.currentTarget; //height of scroll -> depends of amount of data in div
 
-    // console.log(
-    //   scrollHeight.scrollHeight - positionOfScrollTop.scrollTop, // we know that we are in the bottom of the div -> load more data
-    //   clientHeight.clientHeight
-    // );
+    console.log(
+      scrollHeight.scrollHeight - positionOfScrollTop.scrollTop, // we know that we are in the bottom of the div -> load more data
+      clientHeight.clientHeight
+    );
 
-    if (scrollHeight.scrollHeight - positionOfScrollTop.scrollTop === clientHeight.clientHeight) {
+    if (scrollHeight.scrollHeight - positionOfScrollTop.scrollTop <= clientHeight.clientHeight) {
       setPage(page + 1);
-      console.log(page);
     } else {
     }
   };
 
   useEffect(() => {
-    const loadUsers = async () => {
-      setLoading(true);
-      const newUsers = await getUsers(page);
-      setUsers((prev) => [...prev, ...newUsers]);
-
-      getUsers(page);
-      // `https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=5`
-      //`https://randomuser.me/api/?page=${page}&results=60`
-      setLoading(false);
-    };
-
-    loadUsers();
+    getLaunches(page);
   }, [page]);
 
-  const getUsers = async (page: number) => {
-    const users: any = await (
-      await fetch(`https://randomuser.me/api/?page=${page}&results=10`)
-    ).json();
-
-    return users.results;
+  const getLaunches = (page: number) => {
+    setLoading(true);
+    fetch(`https://api.spacex.land/graphql/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `{
+        launchesPast(limit: ${page * 10}) {
+          mission_name
+          id
+          launch_year
+        }
+      }`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLaunches(data.data.launchesPast);
+        setLoading(false);
+      });
   };
 
   return (
     <div className="App">
-      <div className="users" onScroll={handleScroll}>
-        {users &&
-          users.map((user) => (
-            <div style={{ marginBottom: "100px" }} key={user.cell}>
-              {user.cell}
-            </div>
-          ))}
+      <div className="app-overlay"></div>
+      <div className="launches-container">
+        <h1>Launches</h1>
+        <div className="launches" onScroll={handleScroll}>
+          {launches &&
+            launches.map((launch) => (
+              <div key={launch.id} className="launches-element">
+                <IoRocket className="rocket-icon" />
+                <div className="launches-content">
+                  <h1> {launch.mission_name}</h1>
+                  <p> Launch year: {launch.launch_year}</p>
+                </div>
+              </div>
+            ))}
+          {<p style={loading === true ? { display: "block" } : { display: "none" }}>Loading...</p>}
+        </div>
       </div>
-      {loading && <p>Loading...</p>}
     </div>
   );
 }
