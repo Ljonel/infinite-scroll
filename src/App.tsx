be6579c3e015b116897d3364a0896b9555ea51e1
Damian Cygan <damiancygan@MacBook-Pro-Damian.local>
2022-03-30 16:26:57 +0200

    infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+// import GetFromAPI from './getFromAPI';
+
+const GRAPH_QUERY = `
+  {
+    launchesPast(limit: 10) {
+      mission_name
+      id
+      rocket {
+        rocket_name
+        rocket_type
+      }
+    }
+  `;
 
 function App() {
+  const [page, setPage] = useState(0);
+  const [users, setUsers] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const handleScroll = (e: any) => {
+    const positionOfScrollTop = e.currentTarget; //position of scroll
+    const clientHeight = e.currentTarget; //size of client screen
+    const scrollHeight = e.currentTarget; //height of scroll -> depends of amount of data in div
+
+    // console.log(
+    //   scrollHeight.scrollHeight - positionOfScrollTop.scrollTop, // we know that we are in the bottom of the div -> load more data
+    //   clientHeight.clientHeight
+    // );
+
+    if (scrollHeight.scrollHeight - positionOfScrollTop.scrollTop === clientHeight.clientHeight) {
+      setPage(page + 1);
+      console.log(page);
+    } else {
+    }
+  };
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      setLoading(true);
+      const newUsers = await getUsers(page);
+      setUsers((prev) => [...prev, ...newUsers]);
+
+      getUsers(page);
+      // `https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=5`
+      //`https://randomuser.me/api/?page=${page}&results=60`
+      setLoading(false);
+    };
+
+    loadUsers();
+  }, [page]);
+
+  const getUsers = async (page: number) => {
+    const users: any = await (
+      await fetch(`https://randomuser.me/api/?page=${page}&results=10`)
+    ).json();
+
+    return users.results;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="users" onScroll={handleScroll}>
+        {users &&
+          users.map((user) => (
+            <div style={{ marginBottom: "100px" }} key={user.cell}>
+              {user.cell}
+            </div>
+          ))}
+      </div>
+      {loading && <p>Loading...</p>}
     </div>
   );
 }
-
 export default App;
